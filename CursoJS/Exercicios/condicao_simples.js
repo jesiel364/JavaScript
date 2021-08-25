9cc33864362f9c75a6bcb57da8f1f8b68994bcd9
jesiel364 <82860253+jesiel364@users.noreply.github.com>
2021-08-24 23:38:19 -0400

    Add files via upload

---
+++ CursoJS/Exercicios/condicao_simples.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`Sua velocidade é de ${vel}km/h`)
+if (vel > 60){
+  console.log('Você excedeu o limite de velocidade estabelido. MULTADO!')
+}
+console.log('Não se esqueça de colocar o cinto de segurança ao digirir!')
