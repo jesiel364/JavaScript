a340448ae018e0418a8eca6f8425920d25ca3b5c
jesiel364 <82860253+jesiel364@users.noreply.github.com>
2021-09-07 01:49:15 -0400

    Add files via upload

---
+++ CursoJS/Exercicios/repeticoes/repeticoes.js
@@ -0,0 +1,15 @@
+var c = 1
+do { 								// Faca
+	console.log('Olá, tudo bem?')	// isso
+	c++
+} while(c <= 6)						// Enquanto c for menor ou igual a 6
+
+
+
+
+
+// var count = 1
+// while (count <= 500){ // Enquanto count for menor ou igual a 6
+// 	console.log(`Passo ${count}`)
+// 	count ++
+// }
